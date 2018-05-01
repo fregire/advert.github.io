d94c37c44b1f0a3e938f3728cb5a4230e5c8ce8b
fregire <fregire@mail.ru>
2018-05-01 14:07:22 +0500

    Активировал слайдеры

--- src/js/main.js
+++ src/js/main.js
@@ -24,3 +24,9 @@ $(".price__descr .descr__close").on("click", function() {
 		$(".price__descr").removeAttr("style");
 	}, 1000);
 });
+
+// Слайдеры для отзывов и шагов
+$(".feed-item__slider").slick({
+	nextArrow: "<div class='feed-item__arrow feed-item__arrow--next'><svg width='10' height='20' xmlns='http://www.w3.org/2000/svg'><path fill='none' stroke='#000' stroke-miterlimit='10' d='M0 20l10-10M0 0l10 10.7'/></svg></div>",
+	prevArrow: "<div class='feed-item__arrow feed-item__arrow--prev'></div>"
+});
