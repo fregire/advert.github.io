4462168861d3522d7a59c6cc5b412a4e46f68f1b
fregire <fregire@mail.ru>
2018-05-02 16:59:12 +0500

    Исправил меню

--- src/js/main.js
+++ src/js/main.js
@@ -81,7 +81,7 @@ $(".menu__mobile").on("click", function() {
 	$(".menu__list").slideToggle(300, function() {
 		if($(this).css("display") === "none"){
 			$(this).removeAttr("style");
-		}
+		} 
 	});
 	
 });
