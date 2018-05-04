069487a32bfd194d32b1e7f0fd8e1363ae611e46
fregire <fregire@mail.ru>
2018-05-04 18:16:00 +0500

    defw

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,14 @@ $(".projects__slider").slick({
 	prevArrow: '<svg class="projects__arrow projects__arrow--prev" width="11" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11.5 20.1"><path class="projects__arrow-path" fill="none" stroke="#828181" stroke-miterlimit="10" d="M10.2 19.5L.9 10.2 10.2.5"/></svg>',
     nextArrow: '<svg class="projects__arrow projects__arrow--next" width="11" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10.6 19.9"><path class="projects__arrow-path" fill="none" stroke="#828181" stroke-miterlimit="10" d="M.5.4l9.3 9.3-9.3 9.8"/></svg>'
 });
+
+// Слайдеры для отзывов и шагов
+$(".feed-item__slider").slick({
+	slidesToShow: 1,
+	nextArrow: "<div class='feed-item__arrow feed-item__arrow--next'><svg width='10' height='20' xmlns='http://www.w3.org/2000/svg'><path fill='none' stroke='#000' stroke-miterlimit='10' d='M0 20l10-10M0 0l10 10.7'/></svg></div>",
+	prevArrow: "<div class='feed-item__arrow feed-item__arrow--prev'><svg width='10' height='20' xmlns='http://www.w3.org/2000/svg'><path fill='none' stroke='#000' stroke-miterlimit='10' d='M10 0L0 10m10 10L0 9.3'/></svg></div>"
+});
+
 // Добавление счетчика для слайдов
 $(".projects__slider").on('afterChange', function(event, slick, currentSlide){
  	var elemForCount = $(this).siblings(".projects__count-slide");
@@ -24,13 +32,6 @@ $(".price__descr .descr__close").on("click", function() {
 	setInterval(function() {
 		$(".price__descr").removeAttr("style");
 	}, 1000);
-});
-
-// Слайдеры для отзывов и шагов
-$(".feed-item__slider").slick({
-	slidesToShow: 1,
-	nextArrow: "<div class='feed-item__arrow feed-item__arrow--next'><svg width='10' height='20' xmlns='http://www.w3.org/2000/svg'><path fill='none' stroke='#000' stroke-miterlimit='10' d='M0 20l10-10M0 0l10 10.7'/></svg></div>",
-	prevArrow: "<div class='feed-item__arrow feed-item__arrow--prev'><svg width='10' height='20' xmlns='http://www.w3.org/2000/svg'><path fill='none' stroke='#000' stroke-miterlimit='10' d='M10 0L0 10m10 10L0 9.3'/></svg></div>"
 });
 
 // Открытие вопросов в секции faq 
@@ -127,6 +128,7 @@ $(".btn--header, .btn--main-screen, .price__call-to-action").on("click", functio
 	$("html, body").css("overflow", "hidden");
 });
 
+// Закрытие модального окна с успешной отправкой письма
 $(".page").on("click", function() {
 	$(".modal__success").fadeOut();
 });
